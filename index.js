57cc6298b6c79edadc0e3f9ee9b3e66acffb8783
Tonny Bett <betttonny26@gmail.com>
2024-07-21 02:13:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const hqlocation=42;
+function distanceFromHqInBlocks(street){
+    let blocksFromHq=street-hqlocation;
+    return Math.abs(blocksFromHq);
+
+}
+function distanceFromHqInFeet(street){
+    return distanceFromHqInBlocks(street)*264
+}
+function distanceTravelledInFeet(start,destination){
+    return Math.abs(destination-start)*264
+}
+function calculatesFarePrice(start,destination)
+{let totalFeet=distanceTravelledInFeet(start,destination);
+    if(totalFeet<=400){
+        return 0
+    }else if(totalFeet>=400 && totalFeet<=2000){
+        return (totalFeet-400) *0.02
+    }else if (totalFeet>2000 && totalFeet<=2500){
+        
+    return 25
+    }
+
+        
+    else if(totalFeet>2500){
+        return 'cannot travel that far';
+
+    }
+}
